cc16f073ce5259e5e277312ea32e89f8f8d6b126
godL <547188371@qq.com>
2020-12-19 13:10:45 +0800

    clean with async to subpath

--- Sources/XcodeClean/main.swift
+++ Sources/XcodeClean/main.swift
@@ -24,8 +24,9 @@ struct Clean: ParsableCommand {
         
         print(">>>>>>>>>> Counting the size before cleaning <<<<<<<<<<<")
         let beginXcodeSize = try getDirectorySize(at: xcodePath.path)
+        let beginXgpgSize = try getDirectorySize(at: xcpgDevicePath.path)
         let beginSimulatorSize = try getDirectorySize(at: simulatorPath.path)
-        let beginTotal = Double(beginXcodeSize + beginSimulatorSize) / 1024.0 / 1024.0
+        let beginTotal = Double(beginXcodeSize + beginXgpgSize + beginSimulatorSize) / 1024.0 / 1024.0
         
         print(">>>>>>>>>> size: \(beginTotal) MB")
         print(">>>>>>>>>> begin clean <<<<<<<<<<")
@@ -33,16 +34,17 @@ struct Clean: ParsableCommand {
         let group = DispatchGroup()
         let queue = DispatchQueue(label: "com.xclean.GodL", qos: .userInitiated, attributes: .concurrent, autoreleaseFrequency: .inherit, target: nil)
 
-        clean(at: devicePath.path, group: group, queue: queue)
-        clean(at: deviceSupportPath.path, group: group, queue: queue)
-        clean(at: xcpgDevicePath.path, group: group, queue: queue)
-        clean(at: simulatorDevicePath.path, group: group, queue: queue)
+        try clean(at: devicePath.path, group: group, queue: queue)
+        try clean(at: deviceSupportPath.path, group: group, queue: queue)
+        try clean(at: xcpgDevicePath.path, group: group, queue: queue)
+        try clean(at: simulatorDevicePath.path, group: group, queue: queue)
         group.wait()
         do {
             let endXcodeSize = try getDirectorySize(at: xcodePath.path)
+            let endXcpgSize = try getDirectorySize(at: xcpgDevicePath.path)
             let endSimulatorSize = try getDirectorySize(at: simulatorPath.path)
             
-            let total = (beginXcodeSize - endXcodeSize) + (beginSimulatorSize - endSimulatorSize)
+            let total = (beginXcodeSize - endXcodeSize) + (beginSimulatorSize - endSimulatorSize) + (beginXgpgSize - endXcpgSize)
                         
             print(">>>>>>>>>> It's cleaned up altogether \(Double(total) / 1024.0 / 1024.0) MB <<<<<<<<<<")
         }catch let error {
@@ -50,26 +52,32 @@ struct Clean: ParsableCommand {
         }
     }
     
-    func clean(at path: String, group: DispatchGroup, queue: DispatchQueue) {
+    func clean(at path: String, group: DispatchGroup, queue: DispatchQueue) throws {
         guard FileManager.default.fileExists(atPath: path) else {
             print("there are no file at \(path)")
             return
         }
-        group.enter()
-        queue.async(group: group, qos: .userInitiated, flags: .assignCurrentContext) {
-            do {
-                print(">>>>>>>>>> cleaning at \(path) <<<<<<<<<<")
-                try FileManager.default.removeItem(atPath: path)
-                group.leave()
-                print(">>>>>>>>>> clean finished \(path) <<<<<<<<<<")
-            }catch let error {
-                group.leave()
-                print(">>>>>>>>>> cleaning failed \(path) with Error: \(error) <<<<<<<<<<")
-            }
+        try FileManager.default.contentsOfDirectory(atPath: path).forEach {
+            let subPath = path + "/" + $0
+            group.enter()
+            queue.async(group: group, execute: DispatchWorkItem(block: {
+                do {
+                    print(">>>>>>>>>> cleaning at \(subPath) <<<<<<<<<<")
+                    try FileManager.default.removeItem(atPath: subPath)
+                    group.leave()
+                    print(">>>>>>>>>> clean finished \(subPath) <<<<<<<<<<")
+                }catch let error {
+                    group.leave()
+                    print(">>>>>>>>>> cleaning failed \(subPath) with Error: \(error) <<<<<<<<<<")
+                }
+            }))
         }
     }
     
     func getDirectorySize(at directory: String) throws -> UInt64 {
+        guard FileManager.default.fileExists(atPath: directory) else {
+            return 0
+        }
         var size: UInt64 = 0
         try FileManager.default.subpathsOfDirectory(atPath: directory).forEach {
             let subSize = (try FileManager.default.attributesOfItem(atPath: directory.appending("/\($0)")) as NSDictionary).fileSize()
